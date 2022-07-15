b2884f0bfe2c96783e275645239f16b54ccccd93
Herrera <herrera.cesar.arg@outlook.com>
2022-07-15 10:23:39 -0300

    Add touch

--- js/main.js
+++ js/main.js
@@ -132,6 +132,14 @@ comenzar.addEventListener('click', () => {
     // AVANCE SCROLL
     let progress = 24000;
     let EventByDevice
+    console.log(navigator.userAgent);
+    console.log(navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i));
     if (navigator.userAgent.match(/Android/i)
     || navigator.userAgent.match(/webOS/i)
     || navigator.userAgent.match(/iPhone/i)
